132ebb78b0d791a274e2089184cd72e2a5955e09
Douglas Gavioli <douglasbgavioli@hotmail.com>
2026-03-11 10:58:36 -0300

    feat: analytcs vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react"
 import * as C from "./contexts";
 
 import Footer from "./components/Footer";
@@ -39,6 +40,7 @@ function App() {
                     </main>
 
                     <Footer />
+                    <Analytics/>
                   </div>
 
                 </C.MessageProvider>
